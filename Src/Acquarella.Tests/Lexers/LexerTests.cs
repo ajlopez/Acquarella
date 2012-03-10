a2fa9f6521a700cd801ad5e00bb1f82b5b2f3176
ajlopez <webmaster@ajlopez.com>
2012-03-10 10:21:46 -0300

    Get Name with test

---
+++ Src/Acquarella.Tests/Lexers/LexerTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Acquarella.Tests.Lexers
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Acquarella.Lexers;
+
+    [TestClass]
+    public class LexerTests
+    {
+        private Lexer lexer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.lexer = new Lexer();
+        }
+
+        [TestMethod]
+        public void GetNoTokenOnEmptyString()
+        {
+            Assert.AreEqual(0, this.lexer.GetTokens(string.Empty).Count());
+        }
+
+        [TestMethod]
+        public void GetNoTokenOnBlankString()
+        {
+            Assert.AreEqual(0, this.lexer.GetTokens("   ").Count());
+        }
+
+        [TestMethod]
+        public void GetNameToken()
+        {
+            Lexer lexer = new Lexer();
+
+            var result = lexer.GetTokens("name").ToList();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Count);
+
+            var token = result[0];
+
+            Assert.AreEqual("name", token.Value);
+            Assert.AreEqual(TokenType.Name, token.Type);
+        }
+    }
+}
--- Src/Acquarella/Lexers/Lexer.cs
+++ Src/Acquarella/Lexers/Lexer.cs
@@ -7,5 +7,48 @@
 
     public class Lexer
     {
+        private int position;
+        private int length;
+        private string text;
+
+        public IEnumerable<Token> GetTokens(string text)
+        {
+            this.position = 0;
+            this.text = text;
+            this.length = text.Length;
+
+            Token token;
+
+            while ((token = this.GetNextToken()) != null)
+                yield return token;
+        }
+
+        private Token GetNextToken()
+        {
+            this.SkipWhiteSpaces();
+
+            if (this.position >= this.length)
+                return null;
+
+            int start = this.position;
+
+            while (this.position < this.length && !this.IsWhiteSpace(this.text[this.position]))
+                this.position++;
+
+            int length = this.position - start;
+
+            return new Token(TokenType.Name, this.text, start, length);
+        }
+
+        private void SkipWhiteSpaces()
+        {
+            while (this.position < this.length && this.IsWhiteSpace(this.text[this.position]))
+                this.position++;
+        }
+
+        private bool IsWhiteSpace(char ch)
+        {
+            return char.IsWhiteSpace(ch);
+        }
     }
 }
