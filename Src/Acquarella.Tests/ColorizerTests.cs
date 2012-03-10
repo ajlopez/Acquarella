4f99ccf30710e87af2394b0ef66a935f8d48db8f
ajlopez <webmaster@ajlopez.com>
2012-03-10 11:38:07 -0300

    Colorizer with first tests

---
+++ Src/Acquarella.Tests/ColorizerTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Acquarella.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Acquarella.Lexers;
+
+    [TestClass]
+    public class ColorizerTests
+    {
+        private Colorizer colorizer;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.colorizer = new Colorizer();
+        }
+
+        [TestMethod]
+        public void GetColorTypesForSimpleName()
+        {
+            var result = this.colorizer.GetColorTypes("name", new Lexer());
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Count());
+            Assert.IsTrue(result.All(r => r == TokenType.Name));
+        }
+
+        [TestMethod]
+        public void GetColorTypesForTwoNames()
+        {
+            var result = this.colorizer.GetColorTypes("my name", new Lexer());
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(6, result.Where(r => r == TokenType.Name).Count());
+            Assert.AreEqual(1, result.Where(r => r == TokenType.Space).Count());
+            Assert.AreEqual(7, result.Count());
+        }
+    }
+}
---
+++ Src/Acquarella/Colorizer.cs
@@ -0,0 +1,39 @@
+﻿namespace Acquarella
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Acquarella.Lexers;
+
+    public class Colorizer
+    {
+        public IEnumerable<TokenType> GetColorTypes(string text, Lexer lexer)
+        {
+            int position = 0;
+
+            foreach (var token in lexer.GetTokens(text))
+            {
+                while (position < token.Start)
+                {
+                    yield return TokenType.Space;
+                    position++;
+                }
+
+                while (position < token.Start + token.Length)
+                {
+                    yield return token.Type;
+                    position++;
+                }
+            }
+
+            int length = text.Length;
+
+            while (position < length)
+            {
+                yield return TokenType.Space;
+                position++;
+            }
+        }
+    }
+}
