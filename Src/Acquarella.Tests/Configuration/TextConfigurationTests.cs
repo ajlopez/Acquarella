8af69b536576d2eb93f3fce4430c3b91efcbd86f
ajlopez <webmaster@ajlopez.com>
2012-03-11 11:32:55 -0300

    TextConfiguration with first test

---
+++ Src/Acquarella.Tests/Configuration/TextConfigurationTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Acquarella.Tests.Configuration
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Acquarella.Configuration;
+
+    [TestClass]
+    public class TextConfigurationTests
+    {
+        [TestMethod]
+        public void SetAndGetFormat()
+        {
+            TextConfiguration config = new TextConfiguration();
+
+            config.SetFormat("Keyword", "<keyword>");
+
+            Assert.IsNotNull(config.Formats);
+            Assert.AreEqual(1, config.Formats.Count);
+            Assert.AreEqual("<keyword>", config.Formats["Keyword"]);
+        }
+    }
+}
---
+++ Src/Acquarella/Configuration/TextConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Acquarella.Configuration
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class TextConfiguration
+    {
+        private IDictionary<string, string> formats = new Dictionary<string, string>();
+
+        public IDictionary<string, string> Formats { get { return this.formats; } }
+
+        public void SetFormat(string name, string format)
+        {
+            this.formats[name] = format;
+        }
+    }
+}
